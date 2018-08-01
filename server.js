712299d4f4afcf8a4fb3612bd0d70c1e1d344edc
nickbrenn <nbren@tutanota.com>
2018-08-01 17:56:01 -0400

    Made css prettier and made a way to filter out non-word-definition results from daijisen

--- server.js
+++ server.js
@@ -15,12 +15,21 @@ server.get("/api/results/:searchTerm", (req, res) => {
     body
   ) {
     if (!error && response.statusCode == 200) {
-      // const regex = /デジタル大辞泉<\/a>[^!]*/;
-      // const regex = /デジタル大辞泉<\/a>[^!]*![^!]*/im;
-      // const regex = /デジタル大辞泉<\/a><span>の解説[^!]+/gimu;
-      // const regex = /デジタル大辞泉<\/a>[^!]+<![^!]+<!-- \/\.source -->/gi;
+      // this would get all h2's preceding an ex cf div
+      // /<h2>([^\r]*?)<\/h2>(?=[ \n	]*<div class="ex cf">)/g
 
-      // this one is to allow the html string to convert directly
+      // this gets all h3's that come after an ex cf div
+      // /(?<=<div class="ex cf">[\n 	]*)<h3>([^\r]*?)<\/h3>/g
+
+      // this one will select everything in an ex cf that has an h3 that includes kanjikoumoku (excluding opening div)
+      // /(?<=<div class="ex cf">[\n 	]*)<h3>([^［<]*?)［漢字項目］([^\r]*?)<!-- \/\.ex 解説 -->/g
+
+      // this one is same as above but it'll select the opening div so you can delete the whole thing
+      // /<div class="ex cf">[\n 	]*<h3>([^［<]*?)［漢字項目］([^\r]*?)<!-- \/\.ex 解説 -->/g
+
+      // the digital daijisen also uses things like ［列車］［書名］and ［地名］ so make things for thsoe too later
+      // daijirin uses a different format
+
       const daijisenRegex = /デジタル大辞泉<\/a>([^\r]*?)(?=<!-- \/\.dictype 辞書ひとつ -->)/;
       const daijirinRegex = /大辞林 第三版<\/a>([^\r]*?)(?=<!-- \/\.dictype 辞書ひとつ -->)/;
 
@@ -30,10 +39,14 @@ server.get("/api/results/:searchTerm", (req, res) => {
       const daijisenDefinition = body.match(daijisenRegex);
       const daijirinDefinition = body.match(daijirinRegex);
 
-      let match = "";
+      // add settings later to allow the user to decide what to filter out
+      const removeKanjiKoumokuRegex = /<div class="ex cf">[\n 	]*<h3>([^［<]*?)［漢字項目］([^\r]*?)<!-- \/\.ex 解説 -->/g;
+      const removeNonDefinitions = /<div class="ex cf">[\n 	]*<h3>([^［<]*?)［([^\r]*?)<!-- \/\.ex 解説 -->/g;
+
+      let result = "";
 
       if (daijisenDefinition) {
-        match += daijisenDefinition[0]
+        result += daijisenDefinition[0]
           .replace(
             replaceHeaderRegex,
             "<article id='daijisen' class='definition'><h2>大辞泉</h2>"
@@ -42,7 +55,7 @@ server.get("/api/results/:searchTerm", (req, res) => {
       }
 
       if (daijirinDefinition) {
-        match += daijirinDefinition[0]
+        result += daijirinDefinition[0]
           .replace(
             replaceHeaderRegex,
             "<article id='daijirin' class='definition'><h2>大辞林</h2>"
@@ -50,7 +63,9 @@ server.get("/api/results/:searchTerm", (req, res) => {
           .replace(replaceSourceRegex, "");
       }
 
-      res.status(200).send(match.replace(/\n/g, ""));
+      result = result.replace(removeNonDefinitions, "");
+
+      res.status(200).send(result.replace(/\n/g, ""));
     } else {
       console.log("ERROR");
       if (response) {
