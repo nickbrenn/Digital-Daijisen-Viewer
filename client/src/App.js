4caa5918de6cd232de0f9bb938a2a3c9e34e95b7
nickbrenn <nbren@tutanota.com>
2018-10-02 05:35:50 -0400

    Redid the start page to give an example via link to see how it functions if the user cant type in Japanese

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     super();
     this.state = {
       searchInput: "",
-      results: ""
+      results: "<h3>Search something dude...</h3>"
     };
   }
 
@@ -27,6 +27,11 @@ export default class App extends Component {
     // } else
     if (currentUrl.includes("word/")) {
       this.fetchResults(currentUrl.replace(/https?:\/\/[^\/]*\/[a-z]*\//i, ""));
+    } else {
+      const link = currentUrl + "word/辞書";
+      this.setState({
+        results: `<div><h3>Look up a Japanese word using the searchbar.</h3><div>Click this for example results: <a href=${link}>辞書</a></div></div>`
+      });
     }
   };
 
@@ -40,23 +45,21 @@ export default class App extends Component {
     } else {
       this.setState({
         searchInput: searchTerm,
-        results: "<h3 class='result' style='padding-top: 3%'>Loading...</h3>"
+        results: "<h3>Loading...</h3>"
       });
       axios
-        .get(
-          `${baseUrl}/api/results/${searchTerm}`
-        )
+        .get(`${baseUrl}/api/results/${searchTerm}`)
         .then(response => {
-          if(response.data.error && alreadyResent !== true){
+          if (response.data.error && alreadyResent !== true) {
             console.log("There's an error");
             const okuriganaRemoved = this.removeOkurigana(searchTerm);
-            if(okuriganaRemoved !== null){
+            if (okuriganaRemoved !== null) {
               window.location.href = okuriganaRemoved;
-              this.fetchResults(okuriganaRemoved, true) 
+              this.fetchResults(okuriganaRemoved, true);
             } else {
-              this.setState({ results: response.data.error })
+              this.setState({ results: response.data.error });
             }
-          } else if(response.data.error){
+          } else if (response.data.error) {
             this.setState({ results: response.data.error });
           } else {
             this.setState(() => ({ results: response.data }));
@@ -65,42 +68,44 @@ export default class App extends Component {
         .catch(error => {
           console.error("Server Error!!!: ", error);
           this.setState(() => ({
-            results:
-              "<h3 class='result' style='padding-top: 3%'>Error from .catch</h3>"
+            results: "<h3>Error from .catch</h3>"
           }));
         });
     }
   };
 
-  whatCharType = (char) => {
+  whatCharType = char => {
     const hiraganaRange = /[\u3040-\u309f]/;
     const katakanaRange = /[\u30a0-\u30ff]/;
     const kanjiRange = /[\u4e00-\u9faf]/;
-    if(char.match(hiraganaRange) !== null){
+    if (char.match(hiraganaRange) !== null) {
       return "hiragana";
-    } else if(char.match(katakanaRange) !== null){
+    } else if (char.match(katakanaRange) !== null) {
       return "katakana";
-    } else if(char.match(kanjiRange) !== null){
+    } else if (char.match(kanjiRange) !== null) {
       return "kanji";
     } else return null;
-  }
-  
-  removeOkurigana = (str) => {
+  };
+
+  removeOkurigana = str => {
     // as it stands this needs to be decoded
     str = decodeURIComponent(str);
-    for(let i = 0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
       // If the first character isn't kanji, these rules don't apply so return
-      if(i === 0 && this.whatCharType(str[i]) !== "kanji"){
+      if (i === 0 && this.whatCharType(str[i]) !== "kanji") {
         return null;
       } // else if the second/third character is a hiragana in between two kanji, it will return input minus this optional okurigana
-      else if(i >= 1 && this.whatCharType(str[i]) === "hiragana" && 
-        str[i + 1]
-        && this.whatCharType(str[i + 1]) === "kanji"){
+      else if (
+        i >= 1 &&
+        this.whatCharType(str[i]) === "hiragana" &&
+        str[i + 1] &&
+        this.whatCharType(str[i + 1]) === "kanji"
+      ) {
         return str.substr(0, i) + str.substr(i + 1);
       }
     }
     return null;
-  }
+  };
 
   render() {
     return (
@@ -113,19 +118,12 @@ export default class App extends Component {
                 return <Search {...props} fetchResults={this.fetchResults} />;
               }}
             />
-            <Route
-              exact
-              path="/"
-              render={() => {
-                return <div className="result">Search something dude</div>;
-              }}
-            />
           </Col>
         </Row>
         <Row>
           <Col>
             <Route
-              path={`/word/`}
+              path="/"
               render={props => {
                 return <Result {...props} results={this.state.results} />;
               }}
