7f1decce68741cef861938f91e1f410c1e507fd6
nickbrenn <nbren@tutanota.com>
2018-10-19 12:52:26 -0400

    Began adding a way to download definitions in a flashcard compatible format

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -2,8 +2,24 @@ import React from "react";
 
 const Result = props => {
   const html = props.results;
+  const downloadCsv = () => {
+    // const definitions = document.querySelectorAll(".definition");
+    const words = document.getElementsByTagName("h3");
+    const definitions = document.querySelectorAll(".description");
+    const csvData = new Array(words.length - 1);
+    for (let i = 0; i < words.length; i++) {
+      csvData[i] = {
+        word: words[i].innerHTML,
+        definition: definitions[i].innerHTML
+      };
+    }
+    console.log(csvData);
+  };
   return (
-    <div className="result" dangerouslySetInnerHTML={{ __html: html }} />
+    <div>
+      <button onClick={() => downloadCsv()}>Download CSV Definitions</button>
+      <div className="result" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
   );
 };
 
